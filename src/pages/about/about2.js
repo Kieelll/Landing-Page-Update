699960b31177fe275d3ec333afa081e2cc92b94f
Ezekiel Dela Cruz <130634269+Kieelll@users.noreply.github.com>
2025-06-03 10:11:07 +0800

    Fix build configuration and dependencies

--- src/pages/about/about2.js
+++ src/pages/about/about2.js
@@ -3,9 +3,9 @@ import img1 from "../../assets/images/about-img4.png";
 function About2() {
   return (
     <div className="flex flex-col md:flex-row bg-[#79A476] justify-items-center items-center">
-      <div className="self-stretch w-full md:w-[55%]">
+      <div className="self-stretch w-full md:w-[55%] h-[300px] md:h-auto">
         <img
-          className="h-full w-auto p-0 object-cover object-left"
+          className="h-full w-full p-0 object-cover object-center md:object-left"
           src={img1}
           alt="MRF ZERO WASTE STAFFS"
         />
